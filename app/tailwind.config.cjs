4e4d1b72013fd3ee65028b7479b627fab1b5e80d
Ruben Del Rio <me@rdelrio.com>
2022-09-27 08:30:18 -0400

    improved css base, added additional colors

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +7,35 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        enveritas: {
+          700: '#2e8b57',
+          500: '#44c17b',
+        },
+        jebena: {
+          900: '#002060',
+          800: '#1a3670',
+          700: '#003193',
+          600: '#1a469e',
+          500: '#335aa9',
+          400: '#4d6fb3',
+          300: '#6683be',
+          200: '#8098c9',
+          100: '#99add4',
+        },
+        purple: {
+          700: '#761276'
+        },
+        yellow: {
+          300: '#faed27',
+          500: '#dbb300',
+        },
+        tan: {
+          200: '#f7f2e8',
+        }
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
