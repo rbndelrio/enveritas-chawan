3eb409d0b0ada847fb2b40d99b4dd10a53d80d1d
Ruben Del Rio <me@rdelrio.com>
2022-09-27 17:31:39 -0400

    build optimization

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,29 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://sambitsahoo.com/blog/vite-code-splitting-that-works.html
+import { dependencies } from './package.json';
+function renderChunks(deps: Record<string, string>) {
+  let chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-router-dom', 'react-dom'],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
 })
