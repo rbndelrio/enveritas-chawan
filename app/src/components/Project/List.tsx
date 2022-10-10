abec6e7819eaa1c996c98238e8b99879f4287eda
Ruben Del Rio <me@rdelrio.com>
2022-10-10 19:54:49 -0400

    Implement list reducer in projects view

--- app/src/components/Project/List.tsx
+++ app/src/components/Project/List.tsx
@@ -1,16 +1,19 @@
+import { Fragment } from 'react'
+
 import { Project } from '@chawan/forms'
+import { ListState } from '@chawan/react'
 import {
   ChevronRightIcon,
   PencilIcon
 } from '@heroicons/react/20/solid'
 import { Link } from 'react-router-dom'
 
-export type ProjectWithStatus = Project & {
-  status: 'draft' | 'review' | 'approved' | 'published' | 'archived' | 'deleted'
-}
+type ProjectStatus = 'draft' | 'review' | 'approved' | 'published' | 'archived' | 'deleted'
+type ProjectStatusProp = { status: ProjectStatus }
+export type ProjectWithStatus = Project & ProjectStatusProp
 
 interface Props {
-  projects: Array<ProjectWithStatus>
+  projects: ListState<ProjectWithStatus>
 }
 
 const ACTIVE_STATUSES = [ 'approved', 'published' ]
@@ -19,84 +22,109 @@ const EDIT_STATUSES = [ 'draft', 'review' ]
 const classNames = (...classes: string[]) => classes.filter(Boolean).join(' ')
 
 export const ProjectList = (props: Props) => {
+  const projects = props.projects.data
 
   return (
-    <ul role="list" className="divide-y divide-gray-200 border-b border-gray-200">
-      {props.projects.map((project, i) => (
-        <li
-          key={project.id}
-          className="relative py-5 pl-4 pr-6 hover:bg-gray-50 sm:py-6 sm:pl-6 lg:pl-8 xl:pl-6 delay-[var(--i)]"
-        >
-          <div className="flex items-center justify-between space-x-4">
-            {/* Repo name and link */}
-            <div className="min-w-0 space-y-3">
-              <div className="flex items-center space-x-3">
-                {/* TODO: Better solution than these ternaries */}
-                <span
-                  className={classNames(
-                    ACTIVE_STATUSES.includes(project.status)
-                      ? 'bg-enveritas-500/10'
-                      : EDIT_STATUSES.includes(project.status)
-                        ? 'bg-yellow-500/10'
-                        : 'bg-gray-100'
-                    ,
-                    'h-4 w-4 rounded-full flex items-center justify-center'
-                  )}
-                  aria-hidden="true"
-                >
-                  <span
-                    className={classNames(
-                      ACTIVE_STATUSES.includes(project.status)
-                        ? 'bg-enveritas-500/75'
-                        : EDIT_STATUSES.includes(project.status)
-                          ? 'bg-yellow-400'
-                          : 'bg-gray-400'
-                      ,
-                      'h-2 w-2 rounded-full'
-                    )}
-                  />
-                </span>
+    <ul
+      role="list"
+      className="divide-y divide-gray-200 border-b border-gray-200"
+    >
+      {props.projects.order.map((id, i) => {
+        const project = projects[id]
 
-                <h2 className="text-sm font-medium">
-                  <Link to={`/projects/${project.uuid}`}>
-                    <span className="absolute inset-0" aria-hidden="true" />
-                    {project.name}{' '}
-                    <span className="sr-only">{project.name ? 'Running' : 'Not running'}</span>
-                  </Link>
-                </h2>
-              </div>
-              <Link to={`/projects/${project.uuid}`} className="group relative flex items-center space-x-2.5">
-                <span className="truncate text-sm font-medium text-gray-500 group-hover:text-gray-900">
-                  {project.description}
-                </span>
-              </Link>
-            </div>
-            <div className="sm:hidden">
-              <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-            </div>
+        if (!project) return <Fragment key={id}></Fragment>
 
-            {/* Repo meta info */}
-            <div className="hidden flex-shrink-0 flex-col items-end space-y-3 sm:flex">
-              <p className="flex items-center space-x-4">
-                <Link
-                  to={`/projects/${project.uuid}`}
-                  className="flex items-center relative text-sm font-medium text-gray-500 hover:text-gray-900"
-                >
-                  <PencilIcon
-                    className='h-4 w-4'
-                    aria-hidden="true"
-                  />
-                </Link>
-              </p>
-              <p className="flex space-x-2 text-sm text-gray-500">
-                <span>{project.status}</span>
-                <span aria-hidden="true">&middot;</span>
-                <span>Last Modified {project.modified_on.toLocaleDateString()}</span>
-              </p>
-            </div>
-          </div>
-        </li>
-      ))}
+        return (
+          <li
+            key={id}
+            className="relative py-5 pl-4 pr-6 hover:bg-gray-50 sm:py-6 sm:pl-6 lg:pl-8 xl:pl-6 delay-[var(--i)]"
+          >
+            <ProjectListItem data={project} />
+          </li>
+        )
+      })}
     </ul>
   )
 }
+
+type ProjectProps = { data: ProjectWithStatus }
+const ProjectListItem = ({ data: project }: ProjectProps) => (
+  <div className="flex items-center justify-between space-x-4">
+    {/* Repo name and link */}
+    <div className="min-w-0 space-y-3">
+      <div className="flex items-center space-x-3">
+        <StatusIndicator status={project.status} />
+        <h2 className="text-sm font-medium">
+          <Link to={`/projects/${project.uuid}`}>
+            <span className="absolute inset-0" aria-hidden="true" />
+            {project.name}{' '}
+            <span className="sr-only">{project.name ? 'Running' : 'Not running'}</span>
+          </Link>
+        </h2>
+      </div>
+
+      <Link
+        to={`/projects/${project.uuid}`}
+        className="group relative flex items-center space-x-2.5"
+      >
+        <span className="truncate text-sm font-medium text-gray-500 group-hover:text-gray-900">
+          {project.description}
+        </span>
+      </Link>
+    </div>
+
+    <div className="sm:hidden">
+      <ChevronRightIcon
+        className="h-5 w-5 text-gray-400"
+        aria-hidden="true"
+      />
+    </div>
+
+    {/* Repo meta info */}
+    <div className="hidden flex-shrink-0 flex-col items-end space-y-3 sm:flex">
+      <p className="flex items-center space-x-4">
+        <Link
+          to={`/projects/${project.uuid}`}
+          className="flex items-center relative text-sm font-medium text-gray-500 hover:text-gray-900"
+        >
+          <PencilIcon
+            className='h-4 w-4'
+            aria-hidden="true"
+          />
+        </Link>
+      </p>
+      <p className="flex space-x-2 text-sm text-gray-500">
+        <span>{project.status}</span>
+        <span aria-hidden="true">&middot;</span>
+        <span>Last Modified {project.modified_on.toLocaleDateString()}</span>
+      </p>
+    </div>
+  </div>
+)
+
+const StatusIndicator = ({ status }: ProjectStatusProp) => (
+  <span
+    className={classNames(
+      ACTIVE_STATUSES.includes(status)
+        ? 'bg-enveritas-500/10'
+        : EDIT_STATUSES.includes(status)
+          ? 'bg-yellow-500/10'
+          : 'bg-gray-100'
+      ,
+      'h-4 w-4 rounded-full flex items-center justify-center'
+    )}
+    aria-hidden="true"
+  >
+    <span
+      className={classNames(
+        ACTIVE_STATUSES.includes(status)
+          ? 'bg-enveritas-500/75'
+          : EDIT_STATUSES.includes(status)
+            ? 'bg-yellow-400'
+            : 'bg-gray-400'
+        ,
+        'h-2 w-2 rounded-full'
+      )}
+    />
+  </span>
+)
--- app/src/routes/Projects.tsx
+++ app/src/routes/Projects.tsx
@@ -1,21 +1,26 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useReducer } from 'react'
+
+import { initialListState, listReducer } from '@chawan/react'
 import { ProjectHeader, ProjectList, ProjectWithStatus } from '../components/Project'
 
-type ProjectArray = Array<ProjectWithStatus>
-const projectCache: ProjectArray = [
+const projectCache: ProjectWithStatus[] = [
   { status: 'review', id: 0, name: 'Testing 1', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', usage_limit: 'normal', questions: [], uuid: 'aaaa-aaaa-aaaa-aaaa', created_on: new Date(), modified_on: new Date()},
   { status: 'approved', id: 1, name: 'Project Two', description: 'Beatae ducimus magni voluptate praesentium perferendis obcaecati corporis quidem eius', usage_limit: 'normal', questions: [], uuid: 'bbbb-bbbb-bbbb-bbbb', created_on: new Date(), modified_on: new Date()},
   { status: 'published', id: 2, name: '3-Part Field Study', description: 'Asperiores culpa aliquid odio nostrum nulla et dolore illum, veniam eveniet. Officiis!', usage_limit: 'normal', questions: [], uuid: 'cccc-cccc-cccc-cccc', created_on: new Date(), modified_on: new Date()},
   { status: 'archived', id: 3, name: 'Foreground Interview', description: 'Similique rem neque consequuntur ducimus in perspiciatis.', usage_limit: 'normal', questions: [], uuid: 'dddd-dddd-dddd-dddd', created_on: new Date(), modified_on: new Date()},
   { status: 'deleted', id: 4, name: 'Taken Five', description: 'Unused Draft', usage_limit: 'normal', questions: [], uuid: 'eeee-eeee-eeee-eeee', created_on: new Date(), modified_on: new Date()},
 ]
+
 export const Projects = () => {
-  const [projectsState, setProjectsState] = useState<ProjectArray>([])
+  const [state, dispatch] = useReducer(
+    listReducer<ProjectWithStatus>,
+    initialListState<ProjectWithStatus>(),
+  )
 
   useEffect(() => {
     const fetchUserState = () => {
-      new Promise<ProjectArray>((resolve) => { setTimeout(() => resolve(projectCache), 5) })
-        .then((result) => setProjectsState(result))
+      new Promise<ProjectWithStatus[]>((resolve) => { setTimeout(() => resolve(projectCache), 5) })
+        .then((data) => dispatch({ type: 'import', data }))
         .catch((error) => console.log('Somehow failed to set state'));
     };
 
@@ -25,7 +30,7 @@ export const Projects = () => {
   return (
     <>
       <ProjectHeader />
-      <ProjectList projects={projectsState} />
+      <ProjectList projects={state} />
     </>
   )
 }
