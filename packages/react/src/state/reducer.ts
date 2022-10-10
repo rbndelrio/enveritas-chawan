a63629c4724135b2715a0637f0e446fd23b74b53
Ruben Del Rio <me@rdelrio.com>
2022-10-10 18:39:02 -0400

    Flesh out list reducer

--- packages/react/src/state/reducer.ts
+++ packages/react/src/state/reducer.ts
@@ -1,6 +1,6 @@
 interface ListState<T = unknown> {
   /** Last action performed */
-  prev: string
+  last: ActionType | string
 
   /** Position of active item */
   index: number
@@ -18,23 +18,34 @@ interface ListState<T = unknown> {
   data: Record<number, T | undefined>
 }
 
-export const initialListState: ListState = {
-  prev: 'init',
+// TODO: The typing here could be more intelligent by implementing a 2d tuple matrix
+type ActionType =
+  // Replace global state
+  'init' | 'import' |
+
+  // Pure state mutation
+  'set_active' | 'set_index' | 'set_data' | 'delete_data' |
+
+  // Mutations with side effects
+  'add_item' | 'add_items';
+
+  // ¯\_(ツ)_/¯
+  // string;
+
+interface ListAction<T> extends Omit<Partial<ListState<T>>, 'data'> {
+  /** Action to perform */
+  type: ActionType | string
+  data?: T | Array<T> // This should probably be renamed to payload
+}
+
+export const initialListState = <T = unknown>(): ListState<T> => ({
+  last: 'init',
   index: 0,
   active: 0,
   size: 1,
   order: [ 0 ],
   data: { 0: undefined },
-};
-
-// TODO: The typing here could be more intelligent by implementing a 2d tuple matrix
-interface ListAction<T> extends Omit<Partial<ListState<T>>, 'data'> {
-  /** Action to perform */
-  type: String
-
-  // This should probably be renamed to payload
-  data?: T | Array<T>
-}
+});
 
 export const getListItems = <T = unknown>({ order, data }: ListState<T>) =>
   order
@@ -42,50 +53,93 @@ export const getListItems = <T = unknown>({ order, data }: ListState<T>) =>
     // Note we're not filtering with Boolean because other falsy types could be valid T values
     .filter(item => item !== undefined)
 
+
+/** List Reducer
+ *
+ * This is a reducer for managing the UI state of a list without directly accessing the data within.
+ * This opens the door for item grouping and _potentially_ nested list management
+ *
+ * TODO: add methods to remove items, revisit action/property nomenclature
+ *
+ * @param action.type - Actions
+ * * `init` - Reset to initial state
+ * * `import_items` - Replace state with provided data
+ * * `set_active` - Sets current/focused list item
+ * * `set_index` - Change position of active item
+ * * `set_data` - Replace the data of the current item
+ * * `delete_data` - Sets active item to undefined
+ * * `add_item` - Add single item to list
+ * * `add_items` - Add multiple items to list
+ */
 export function listReducer<
-    T,
+    T, // FIXME: T extending Array could cause bugs with certain actions
     List extends ListState<T>,
     Action extends ListAction<T>
-  >(state: List, action: Action): List {
+  >(state: List, action: Action): ListState<T> {
   const last = action.type
 
   switch (action.type) {
+    case 'init': {
+      return initialListState();
+    }
+    case 'import': {
+      if (!(Array.isArray(action.data) && action.data.length)) return initialListState()
+
+      const length = action.data.length
+
+      const order = Array.from<any>({ length }) // This optimization will save like 5 nanoseconds 🏎💨
+      const newState: ListState<T> = {
+        last,
+        active: length - 1,
+        index: length - 1,
+        size: length,
+        order,
+        data: { ...order },
+      }
+
+      for (let i = 0; i < length; i++) {
+        const item = action.data[i];
+        newState.order[i] = i
+        newState.data[i] = item
+      }
+
+      return newState
+    }
     case 'set_active': {
-      const newActiveId = Math.min(Math.max(action.active ?? 0, 0), state.size - 1)
+      const id = Math.min(Math.max(action.active ?? 0, 0), state.size - 1)
       return {
         ...state,
         last,
-        index: state.order.indexOf(newActiveId),
-        active: newActiveId,
+
+        index: state.order.indexOf(id),
+        active: id,
       };
     }
     case 'set_index': {
-      const fromIndex = state.index
-      const toIndex = Math.min(Math.max(action.index ?? 0, 0), state.order.length - 1)
+      const from = state.index
+      const to = Math.min(Math.max(action.index ?? 0, 0), state.order.length - 1)
       const order = [...state.order]
 
-      order.splice(
-        toIndex,
-        0,
-        ...order.splice(fromIndex, 1)
-      )
+      order.splice(to, 0, ...order.splice(from, 1))
 
       return {
         ...state,
         last,
-        index: toIndex,
+
+        index: to,
         order,
       }
     }
     case 'set_data': {
-      const index = Math.max(0, state.active ?? state.order.length)
+      const id = Math.max(0, state.active ?? state.order.length)
 
       return {
         ...state,
         last,
+
         data: {
           ...state.data,
-          [index]: action.data,
+          [id]: action.data,
         },
       };
     }
@@ -93,11 +147,80 @@ export function listReducer<
       return {
         ...state,
         last,
+
         data: {
           ...state.data,
           [state.active]: undefined,
         },
       };
+    }
+    case 'add_item': {
+      const id = state.size + 1
+      let index
+      let order
+
+      if (Number.isInteger(action.index)) {
+        index = action.index
+        order = [
+          ...state.order.slice(0, index),
+          id,
+          ...state.order.slice(index)
+        ]
+      } else {
+        index = id
+        order = [...state.order, id]
+      }
+
+      return {
+        ...state,
+        last,
+
+        index,
+        size: id,
+        order,
+        data: {
+          ...state.data,
+          [id]: action.data,
+        },
+      }
+    }
+    case 'add_items': {
+      let size = state.size
+
+      // Coerce data into an Item array
+      const items = Array.isArray(action.data) && action.data.length
+        ? action.data
+        : []
+
+      // Incrementing indices based on previous state's size
+      const ids = Array
+        .from({ length: items.length })
+        .map((_, i) => size + 1 + i)
+
+      const order = Number.isInteger(action.index)
+        ? [ // Set new order based on index position
+          ...state.order.slice(0, action.index),
+          ...ids,
+          ...state.order.slice(action.index),
+        ]
+        : [ // Append to the end of the array
+          ...state.order,
+          ...ids,
+        ]
+
+      size += items.length
+
+      return {
+        ...state,
+        last,
+
+        size,
+        order,
+        data: {
+          ...state.data,
+          ...ids.reduce<Record<number, T>>((obj, id, i) => { obj[id] = items[i]; return obj }, {})
+        },
+      }
     }
     default: {
       throw Error('Unknown action: ' + action.type);
